d511f41d34596f0e5d2f3d29af2ba24865375d44
atefa1234 <atefa.mahmoodi2021@gmail.com>
2024-01-06 12:15:50 +0100

    two test have been passed correctly

--- leapyear.js
+++ leapyear.js
@@ -1,16 +1,18 @@
-function leapyear(year){
-    if(year % 400 === 0){
-        return true
+class leapyear{
+    leapyear(year){
+        if(year % 400 === 0){
+            return true
+        }
+        else if(year %300 === 0)
+        {
+            return false
+        }
+        else if(year %4 === 0){
+            return true
+        }else if(year % 4 !== 0){
+            return false
+        }
+    
     }
-    else if(year %300 === 0)
-    {
-        return false
-    }
-    else if(year %4 === 0){
-        return true
-    }else if(year % 4 !== 0){
-        return false
-    }
-
 }
 module.exports = leapyear;
--- test/leapyear.test.js
+++ test/leapyear.test.js
@@ -2,6 +2,18 @@ const {expect} = require ("@jest/globals");
 const leapyear = require("../leapyear");
 
 
-descirbe("tests that validate if a year is leap year or no", ()=>{
-
+describe("tests that validate if a year is leap year or no", ()=>{
+    const leap_year = new leapyear();
+    test('A year is not a leap year if it is not divisible by 4', ()=>{
+        const year_entered = 1997;
+        const expexted_answer = false;
+        const recieved_answer = leap_year.leapyear(year_entered);
+        expect(recieved_answer).toBe(expexted_answer);
+    })
+    test('A year is a leap year if it is divisible by 4', ()=>{
+        const year_entered = 1996;
+        const expected_answer = true;
+        const recieved_answer = leap_year.leapyear(year_entered);
+        expect(recieved_answer).toBe(expected_answer);
+    })
 })
