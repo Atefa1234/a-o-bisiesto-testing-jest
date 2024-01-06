b9c9e79d90f7adc2ae6f4feecf6c01dac895fc2a
atefa1234 <atefa.mahmoodi2021@gmail.com>
2024-01-06 11:57:14 +0100

    javascript codes have been writed

---
+++ leapyear.js
@@ -0,0 +1,16 @@
+function leapyear(year){
+    if(year % 400 === 0){
+        return true
+    }
+    else if(year %300 === 0)
+    {
+        return false
+    }
+    else if(year %4 === 0){
+        return true
+    }else if(year % 4 !== 0){
+        return false
+    }
+
+}
+module.exports = leapyear;
---
+++ test/leapyear.test.js
@@ -0,0 +1,7 @@
+const {expect} = require ("@jest/globals");
+const leapyear = require("../leapyear");
+
+
+descirbe("tests that validate if a year is leap year or no", ()=>{
+
+})
